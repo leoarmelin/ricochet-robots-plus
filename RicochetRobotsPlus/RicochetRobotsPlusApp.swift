7d06fa3f5883ff6d49b3954bc51f6838aa67469d
leoarmelin <leonardo99.armelin@gmail.com>
2023-04-18 21:41:00 -0300

    Initial Commit

---
+++ RicochetRobotsPlus/RicochetRobotsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicochetRobotsPlusApp.swift
+//  RicochetRobotsPlus
+//
+//  Created by Leonardo Armelin on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RicochetRobotsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
